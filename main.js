689853fb0b3306f9275d429d93607ccd86e5dc4d
Gugo <gugo1519@gmail.com>
2020-09-12 14:13:01 +0400

    finish

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    nav:true,
     dots:true,
     responsive:{
         400:{
@@ -15,3 +15,16 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+let webMenu = document.getElementsByClassName('right_thead');
+let menu = document.getElementsByClassName('menu__btn__mobile');
+menu[0].addEventListener('click', mobileMenu, false);
+
+function mobileMenu(){
+    // webMenu[0].classList.add('class', 'menu__mobile');
+    webMenu[0].setAttribute('class', 'menu__mobile');
+       menu[0].setAttribute('class', 'none');
+    webMenu[0].style.alignItems = 'flex-end';
+    // webMenu[0].style.flexDirection = 'column-reverse';
+}
